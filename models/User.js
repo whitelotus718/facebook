48ed318fa432b0965f689f84e9df7529a241b354
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-06-23 19:43:05 -0400

    user post route tested with Postman

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema({
     username:{
         type:String,
-        require: true
+        require: true,
         min:3,
         max:20,
         unique:true
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,20 @@ const router = require("express").Router();
 const User = require("../models/User")
 
 // REGISTER
+router.post("/register", async (req,res) => {
+    const newUser = new User ({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch(err){
+        console.log(err)
+    }
+})
 
 router.get("/", (req,res) => {
     res.send("hey its auth route")
