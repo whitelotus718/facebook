d5a17e15ce200ae7906cfc9654042a2cbfe356d7
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-06-24 22:43:36 -0400

    login route

--- routes/auth.js
+++ routes/auth.js
@@ -25,6 +25,21 @@ router.post("/register", async (req,res) => {
     }
 })
 
+// LOGIN
+router.post("login", (req, res)=>{
+    try {
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).json("user not found")
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password")
+
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err);
+    }
+    
+})
+
 router.get("/", (req,res) => {
     res.send("hey its auth route")
 })
